4e5276f842847469c8753c5cfef428d2371de74d
Pei-Zhu Lai <ht910155@gmail.com>
2025-04-28 09:19:18 +0200

    add HZaTo2l2g cards

---
+++ bin/MadGraph5_aMCatNLO/cards/13p6TeV/HZaTo2l2g/copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+default=$(basename $(pwd))
+# Handle 0.1 to 0.9,  1 to 30
+for m in $(seq 0.1 0.1 0.9) {1..30}
+do
+    # format m file name (Replace . with p)
+    m_formatted=$(echo "$m" | tr '.' 'p')
+    # Keep Mass Point as float
+    mass_value="$m"
+    if [[ $m == [1-9]* ]]; then
+        mass_value="$m.0"
+    fi
+
+    echo Copying mass M$m
+    newdir=../"$default"_M"$m_formatted"
+    mkdir $newdir
+    cp "$default"_customizecards.dat $newdir/"$default"_M"$m_formatted"_customizecards.dat
+    cp "$default"_extramodels.dat $newdir/"$default"_M"$m_formatted"_extramodels.dat
+    cp "$default"_proc_card.dat $newdir/"$default"_M"$m_formatted"_proc_card.dat
+    cp "$default"_run_card.dat $newdir/"$default"_M"$m_formatted"_run_card.dat
+    # modify output name
+    sed -i 's/'$default'/'$default'_M'$m_formatted'/g' $newdir/"$default"_M"$m_formatted"_proc_card.dat
+    # Modify mass parameter
+    sed -i 's/AMASS/'$mass_value'/g' $newdir/"$default"_M"$m_formatted"_customizecards.dat
+done
---
+++ bin/MadGraph5_aMCatNLO/gridpack_generation_run_all_ma_13TeV.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+default=HZaTo2l2g
+# Handle 0.1 to 0.9,  1 to 30
+for m in $(seq 0.1 0.1 0.9)
+do
+    # format m file name (Replace . with p)
+    m_formatted=$(echo "$m" | tr '.' 'p')
+
+    echo Generating Gridpack for mass M$m
+    
+    ./gridpack_generation.sh "$default"_M"$m_formatted" ./cards/13TeV/"$default"_M"$m_formatted"
+
+done
