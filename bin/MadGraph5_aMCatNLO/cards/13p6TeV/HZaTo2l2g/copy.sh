d2971c4e219cb5e84c6ff368417bf32e46497611
Pei-Zhu Lai <ht910155@gmail.com>
2025-06-26 08:56:33 +0200

    Changed decay width and mini lep and photon pT cut

--- bin/MadGraph5_aMCatNLO/cards/13p6TeV/HZaTo2l2g/copy.sh
+++ bin/MadGraph5_aMCatNLO/cards/13p6TeV/HZaTo2l2g/copy.sh
@@ -2,7 +2,7 @@
 
 default=$(basename $(pwd))
 # Handle 0.1 to 0.9,  1 to 30
-for m in $(seq 0.1 0.1 0.9) {1..30}
+for m in $(seq 0.1 0.1 0.9) {1..10} {15,20,25,30}
 do
     # format m file name (Replace . with p)
     m_formatted=$(echo "$m" | tr '.' 'p')
